8c81eb87e81de1c1d144eb8d46478b48ee5e2966
Juliano Wickboldt <julianowick@gmail.com>
2019-05-07 14:39:51 +0000

    Adding extension control to assignment upload. Only zip extension is allowed.

--- plag/models.py
+++ plag/models.py
@@ -1,6 +1,8 @@
 import os
+import logging
 
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 class Course(models.Model):
     name = models.CharField(max_length=254)
@@ -22,7 +24,25 @@ def upload_filename(instance, filename):
 class Assignment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    upload = models.FileField(upload_to=upload_filename, null=True, blank=True)
+    upload = models.FileField(upload_to=upload_filename, null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=('zip',))])
+
+    def extract(self):
+        pass
+
+    def moss(self):
+        pass
 
     def __str__(self):
         return self.name
+
+    class AssignmentException(Exception):
+        # Get an instance of a logger
+        logger = logging.getLogger(__name__)
+
+        def __init__(self, msg):
+            self.msg = msg
+            self.logger.warning(msg)
+
+        def __str__(self):
+            return repr(self.msg)
+

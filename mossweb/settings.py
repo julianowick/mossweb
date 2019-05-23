f455a84c36fa39ba99350bbc5145a3f7bdf99d22
Juliano Wickboldt <julianowick@gmail.com>
2019-05-23 02:55:57 +0000

    Removing some settings from versioning.

--- mossweb/settings.py
+++ mossweb/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4i)mup4qa^z(b^d&-zcg(c%nlbx*f@6d4q$pr&i8r@+8b6l^!x'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pinkfloyd.inf.ufrgs.br', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
